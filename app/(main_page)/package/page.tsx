f95770d84be0f491cb8876fdea0a4828d8bd99a1
dauadonhere <hellodadedaud@gmail.com>
2025-12-20 14:53:47 +0700

    fix: package price

--- app/(main_page)/package/page.tsx
+++ app/(main_page)/package/page.tsx
@@ -6,8 +6,8 @@ export default function PackagePage() {
     <div className="flex flex-col gap-3 p-3 w-full">
 
       <div className="relative overflow-hidden rounded-xl bg-primary-theme p-4 text-background shadow-xl border-2 border-ring/50">
-        <p className="text-lg font-extrabold">100 Mbps</p>
-        <p className="text-xs opacity-90">Rp 100.000 / Bulan</p>
+        <p className="text-lg font-extrabold">12 Mbps</p>
+        <p className="text-xs opacity-90">Rp 120.000 / Bulan</p>
 
         <button
           className="absolute bottom-4 right-4 rounded-lg bg-background px-4 py-1 text-xs font-bold text-primary-theme shadow"
@@ -17,7 +17,7 @@ export default function PackagePage() {
       </div>
 
       <div className="relative overflow-hidden rounded-xl bg-primary-theme p-4 text-background shadow-xl border-2 border-ring/50">
-        <p className="text-lg font-extrabold">150 Mbps</p>
+        <p className="text-lg font-extrabold">15 Mbps</p>
         <p className="text-xs opacity-90">Rp 150.000 / Bulan</p>
 
         <button
@@ -28,7 +28,18 @@ export default function PackagePage() {
       </div>
 
       <div className="relative overflow-hidden rounded-xl bg-primary-theme p-4 text-background shadow-xl border-2 border-ring/50">
-        <p className="text-lg font-extrabold">200 Mbps</p>
+        <p className="text-lg font-extrabold">17 Mbps</p>
+        <p className="text-xs opacity-90">Rp 170.000 / Bulan</p>
+
+        <button
+          className="absolute bottom-4 right-4 rounded-lg bg-background px-4 py-1 text-xs font-bold text-primary-theme shadow"
+        >
+          Pilih
+        </button>
+      </div>
+
+      <div className="relative overflow-hidden rounded-xl bg-primary-theme p-4 text-background shadow-xl border-2 border-ring/50">
+        <p className="text-lg font-extrabold">20 Mbps</p>
         <p className="text-xs opacity-90">Rp 200.000 / Bulan</p>
 
         <button
@@ -37,20 +48,9 @@ export default function PackagePage() {
           Pilih
         </button>
       </div>
-
       <div className="relative overflow-hidden rounded-xl bg-primary-theme p-4 text-background shadow-xl border-2 border-ring/50">
-        <p className="text-lg font-extrabold">10 Mbps</p>
-        <p className="text-xs opacity-90">Rp 10.000 / Jam</p>
-
-        <button
-          className="absolute bottom-4 right-4 rounded-lg bg-background px-4 py-1 text-xs font-bold text-primary-theme shadow"
-        >
-          Pilih
-        </button>
-      </div>
-      <div className="relative overflow-hidden rounded-xl bg-primary-theme p-4 text-background shadow-xl border-2 border-ring/50">
-        <p className="text-lg font-extrabold">10 Mbps</p>
-        <p className="text-xs opacity-90">Rp 30.000 / Hari</p>
+        <p className="text-lg font-extrabold">25 Mbps</p>
+        <p className="text-xs opacity-90">Rp 250.000 / Bulan</p>
 
         <button
           className="absolute bottom-4 right-4 rounded-lg bg-background px-4 py-1 text-xs font-bold text-primary-theme shadow"
@@ -60,8 +60,8 @@ export default function PackagePage() {
       </div>
 
       <div className="relative overflow-hidden rounded-xl bg-primary-theme p-4 text-background shadow-xl border-2 border-ring/50">
-        <p className="text-lg font-extrabold">100 Mbps</p>
-        <p className="text-xs opacity-90">Rp 100.000 / Bulan</p>
+        <p className="text-lg font-extrabold">25 Mbps</p>
+        <p className="text-xs opacity-90">Rp 250.000 / Bulan</p>
 
         <button
           className="absolute bottom-4 right-4 rounded-lg bg-background px-4 py-1 text-xs font-bold text-primary-theme shadow"
@@ -71,40 +71,8 @@ export default function PackagePage() {
       </div>
 
       <div className="relative overflow-hidden rounded-xl bg-primary-theme p-4 text-background shadow-xl border-2 border-ring/50">
-        <p className="text-lg font-extrabold">150 Mbps</p>
-        <p className="text-xs opacity-90">Rp 150.000 / Bulan</p>
-
-        <button
-          className="absolute bottom-4 right-4 rounded-lg bg-background px-4 py-1 text-xs font-bold text-primary-theme shadow"
-        >
-          Pilih
-        </button>
-      </div>
-
-      <div className="relative overflow-hidden rounded-xl bg-primary-theme p-4 text-background shadow-xl border-2 border-ring/50">
-        <p className="text-lg font-extrabold">200 Mbps</p>
-        <p className="text-xs opacity-90">Rp 200.000 / Bulan</p>
-
-        <button
-          className="absolute bottom-4 right-4 rounded-lg bg-background px-4 py-1 text-xs font-bold text-primary-theme shadow"
-        >
-          Pilih
-        </button>
-      </div>
-
-      <div className="relative overflow-hidden rounded-xl bg-primary-theme p-4 text-background shadow-xl border-2 border-ring/50">
-        <p className="text-lg font-extrabold">10 Mbps</p>
-        <p className="text-xs opacity-90">Rp 10.000 / Jam</p>
-
-        <button
-          className="absolute bottom-4 right-4 rounded-lg bg-background px-4 py-1 text-xs font-bold text-primary-theme shadow"
-        >
-          Pilih
-        </button>
-      </div>
-      <div className="relative overflow-hidden rounded-xl bg-primary-theme p-4 text-background shadow-xl border-2 border-ring/50">
-        <p className="text-lg font-extrabold">10 Mbps</p>
-        <p className="text-xs opacity-90">Rp 30.000 / Hari</p>
+        <p className="text-lg font-extrabold">30 Mbps</p>
+        <p className="text-xs opacity-90">Rp 300.000 / Bulan</p>
 
         <button
           className="absolute bottom-4 right-4 rounded-lg bg-background px-4 py-1 text-xs font-bold text-primary-theme shadow"
